c3627591746538d302c77c2f2d70598a6bde4d43
dennyerikson <dennyeriks00on@gmail.com>
2018-07-27 16:45:47 -0300

    add modal-dialog

--- appfat/blog/views.py
+++ appfat/blog/views.py
@@ -4,13 +4,16 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Aluno, Status, Info
 
+consulta = '38019987894'
+
 # metodo para passar dados confiltro para a home
 def home(request):
     # filtro para consulta 
-    post = Post.objects.filter(dis_cpf='38019987894') # informativos curso
-    aluno = Aluno.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #dados aluno
-    status = Status.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #tabela atenção
-    info = Info.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #tabela informação
+    post = Post.objects.filter(dis_cpf=consulta) # informativos curso
+    aluno = Aluno.objects.filter(alu_cpf=consulta) #dados aluno
+    # curso = Post.objects.filter(dis_disciplina = post.dis_disciplina) #dados aluno
+    status = Status.objects.filter(status_cpf=consulta) #tabela atenção
+    info = Info.objects.filter() #tabela informação
 
     #get context dos atributos de .models
     context = {'aluno': aluno, 'status':status, 'info':info, 'post': post}
