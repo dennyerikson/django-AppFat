e3eaf4a660533cf426323eddc42cfbfc323e2c40
dennyerikson <dennyeriks00on@gmail.com>
2018-08-20 16:36:00 -0300

    add layouts boleto, conecta, convenio, curso, info, sala

--- appfat/blog/urls.py
+++ appfat/blog/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
-from .views import home, register
+from .views import home, boleto, curso, sala, conecta, convenio, Info
 
 urlpatterns = [
-    url( '', home),
-    url('register/', register, name='register')
+    # url( '', home),
+    # url('register/', register, name='register')
+
+
+    url('', home),
+    url('/boleto/', boleto),
+    url('/curso/', curso),
+    url('/sala/', sala),
+    url('/conecta/', conecta),
+    url('/convenio/', convenio),
+    url('/info/', Info),
+
 ]
